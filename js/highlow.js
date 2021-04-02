f84a268fc9b4ddf79dec79d2508022d67de451e9
KoGandhi <78721669+hyungin0505@users.noreply.github.com>
2021-04-02 17:19:55 +0900

    UPDATE
    highlow 스크립트 수정

--- js/highlow.js
+++ js/highlow.js
@@ -1,8 +1,8 @@
 var comp=Math.floor(Math.random() * 100);
 var show=Math.floor(Math.random() * 100);
 
-function highlow();
-
+function highlow() {
+    
     answer=prompt('The number selected is '+show+'\nwill the next number be higher or lower?Randomly selects a number from 1-100\n(답은 high 또는 low로 입력해주세요.');
                 
     if(answer == 'high') {       
@@ -24,3 +24,4 @@ function highlow();
     else {
         alert('할거면 하고 안 할거면 말지 으휴 진짜')
     }
+}
