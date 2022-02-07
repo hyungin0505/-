8ec61867c3049fa9bf52977e7f31a284f901cc76
KoGandhi <78721669+hyungin0505@users.noreply.github.com>
2022-02-07 11:18:15 +0900

    파일 추가함
    bb

---
+++ javascript/highlow.js
@@ -0,0 +1,26 @@
+function highlow() {
+    var comp=Math.floor(Math.random() * 100);
+    var show=Math.floor(Math.random() * 100);
+
+    answer=prompt('The number selected is '+show+'\nwill the next number be higher or lower?Randomly selects a number from 1-100\n(답은 high 또는 low로 입력해주세요.');
+                
+    if(answer == 'high') {       
+        if (comp > show) {
+            alert('The number selected was '+comp+', you won');
+        }
+        else if (comp < show) {
+            alert('The number selected was '+comp+', you lose');
+        }
+    }
+    else if(answer == 'low') {
+        if (comp < show) {
+            alert('The number selected was '+comp+', you won');
+        }
+        else if (comp > show) {
+            alert('The number selected was '+comp+', you lose');
+        }
+    }
+    else {
+        alert('할거면 하고 안 할거면 말지 으휴 진짜')
+    }
+}
